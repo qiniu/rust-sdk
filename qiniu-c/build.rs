c92a7662d7566d2a57edf9ee8ee18dce42541ec3
Bachue Zhou <bachue.shu@gmail.com>
2019-08-07 10:23:39 +0800

    add cpp support

--- qiniu-c/build.rs
+++ qiniu-c/build.rs
@@ -5,8 +5,17 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let mut config: Config = Default::default();
     config.language = Language::C;
+    config.documentation = true;
 
     cbindgen::generate_with_config(&crate_dir, config)
         .expect("Unable to generate bindings")
         .write_to_file("libqiniu_ng.h");
+
+    let mut config: Config = Default::default();
+    config.language = Language::Cxx;
+    config.documentation = true;
+    config.namespace = Some("qiniu_ng".to_owned());
+    cbindgen::generate_with_config(&crate_dir, config)
+        .expect("Unable to generate bindings")
+        .write_to_file("libqiniu_ng.hpp");
 }
