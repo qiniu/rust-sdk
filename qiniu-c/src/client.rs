81442dfef9d1cd2585bae67694ed2e8ef19e67d5
Bachue Zhou <bachue.shu@gmail.com>
2019-09-24 15:11:48 +0800

    wrap client::Client to c interfaces

--- qiniu-c/src/client.rs
+++ qiniu-c/src/client.rs
@@ -1,8 +1,22 @@
-// use qiniu::utils::Client;
-// use std::os::raw::c_char;
+use libc::c_void;
+use qiniu::{Client, Config};
+use std::{ffi::CStr, os::raw::c_char};
 
-// #[no_mangle]
-// pub extern "C" fn qiniu_ng_client_new(access_key: *const c_char, secret_key: *const c_char) -> *mut c_void {
-//     let etag = Box::new(Client::new());
-//     Box::into_raw(etag) as usize as *mut c_void
-// }
+#[no_mangle]
+pub extern "C" fn qiniu_ng_client_new(
+    access_key: *const c_char,
+    secret_key: *const c_char,
+    config_ptr: *mut c_void,
+) -> *mut c_void {
+    let client = Box::new(Client::new(
+        unsafe { CStr::from_ptr(access_key) }.to_string_lossy(),
+        unsafe { CStr::from_ptr(secret_key) }.to_string_lossy(),
+        unsafe { *Box::from_raw(config_ptr as usize as *mut Config) },
+    ));
+    Box::into_raw(client) as usize as *mut c_void
+}
+
+#[no_mangle]
+pub extern "C" fn qiniu_ng_client_free(client_ptr: *mut c_void) {
+    unsafe { Box::from_raw(client_ptr as usize as *mut Client) };
+}
