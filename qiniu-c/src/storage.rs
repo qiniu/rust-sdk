c35e4f16c1355c485d63383bdf5a6546c7a2b805
Bachue Zhou <bachue.shu@gmail.com>
2020-02-21 17:38:14 +0800

    document qiniu-c storage

--- qiniu-c/src/storage.rs
+++ qiniu-c/src/storage.rs
@@ -8,6 +8,12 @@ use crate::{
 use qiniu_ng::Client;
 use tap::TapOps;
 
+/// @brief 列出所有存储空间名称
+/// @param[in] client 七牛客户端
+/// @param[out] names 存储空间名称列表，如果传入 `NULL` 表示不获取 `names`。但如果运行正常，返回值将依然是 `true`
+/// @param[out] error 用于返回错误，如果传入 `NULL` 表示不获取 `error`。但如果运行发生错误，返回值将依然是 `false`
+/// @retval bool 是否运行正常，如果返回 `true`，则表示可以读取 `names` 获得结果，如果返回 `false`，则表示可以读取 `error` 获得错误信息
+/// @warning 对于获取的 `names` 或 `error`，一旦使用完毕，应该调用各自的内存释放方法释放内存
 #[no_mangle]
 pub extern "C" fn qiniu_ng_storage_bucket_names(
     client: qiniu_ng_client_t,
@@ -33,6 +39,16 @@ pub extern "C" fn qiniu_ng_storage_bucket_names(
     }
 }
 
+/// @brief 创建存储空间
+/// @param[in] client 七牛客户端
+/// @param[out] bucket_name 存储空间名称
+/// @param[out] region_id 区域 ID，如果您的区域 ID 不是 `qiniu_ng_region_id_t` 所能提供，则调用 `qiniu_ng_storage_create_bucket_with_customized_region_id()` 函数创建存储空间
+/// @param[out] error 用于返回错误，如果传入 `NULL` 表示不获取 `error`。但如果运行发生错误，返回值将依然是 `false`
+/// @retval bool 是否运行正常，如果返回 `true`，则表示创建成功，如果返回 `false`，则表示可以读取 `error` 获得错误信息
+/// @details
+///     在创建存储空间时，需要注意存储空间的名称必须遵守以下规则：
+///     - 存储空间名称不允许重复，遇到冲突请更换名称。
+///     - 名称由 3 ~ 63 个字符组成 ，可包含小写字母、数字和短划线，且必须以小写字母或者数字开头和结尾。
 #[no_mangle]
 pub extern "C" fn qiniu_ng_storage_create_bucket(
     client: qiniu_ng_client_t,
@@ -52,8 +68,18 @@ pub extern "C" fn qiniu_ng_storage_create_bucket(
     })
 }
 
+/// @brief 使用自定义区域 ID 创建存储空间
+/// @param[in] client 七牛客户端
+/// @param[out] bucket_name 存储空间名称
+/// @param[out] region_id 区域 ID 字符串，参考[官方文档](https://developer.qiniu.com/kodo/manual/1671/region-endpoint)
+/// @param[out] error 用于返回错误，如果传入 `NULL` 表示不获取 `error`。但如果运行发生错误，返回值将依然是 `false`
+/// @retval bool 是否运行正常，如果返回 `true`，则表示创建成功，如果返回 `false`，则表示可以读取 `error` 获得错误信息
+/// @details
+///     在创建存储空间时，需要注意存储空间的名称必须遵守以下规则：
+///     - 存储空间名称不允许重复，遇到冲突请更换名称。
+///     - 名称由 3 ~ 63 个字符组成 ，可包含小写字母、数字和短划线，且必须以小写字母或者数字开头和结尾。
 #[no_mangle]
-pub extern "C" fn qiniu_ng_storage_create_bucket2(
+pub extern "C" fn qiniu_ng_storage_create_bucket_with_customized_region_id(
     client: qiniu_ng_client_t,
     bucket_name: *const qiniu_ng_char_t,
     region_id: *const qiniu_ng_char_t,
@@ -88,6 +114,11 @@ fn _qiniu_ng_storage_create_bucket(
     }
 }
 
+/// @brief 删除存储空间
+/// @param[in] client 七牛客户端
+/// @param[out] bucket_name 即将删除的存储空间名称
+/// @param[out] error 用于返回错误，如果传入 `NULL` 表示不获取 `error`。但如果运行发生错误，返回值将依然是 `false`
+/// @retval bool 是否运行正常，如果返回 `true`，则表示删除成功，如果返回 `false`，则表示可以读取 `error` 获得错误信息
 #[no_mangle]
 pub extern "C" fn qiniu_ng_storage_drop_bucket(
     client: qiniu_ng_client_t,
