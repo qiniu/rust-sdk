19d8abeb112cf7a15e043cedb60de7742f7f9b54
Bachue Zhou <bachue.shu@gmail.com>
2020-02-26 12:28:38 +0800

    document qiniu-ruby error

--- qiniu-ruby/lib/qiniu_ng/error.rb
+++ qiniu-ruby/lib/qiniu_ng/error.rb
@@ -1,20 +1,37 @@
 # frozen_string_literal: true
 
 module QiniuNg
+  # SDK 错误类
+  #
+  # 作为所有 SDK 错误类的基类
   class Error < StandardError
+    # @!visibility private
     def inspect
       "#<#{self.class.name}>"
     end
 
+    # IO 异常
     class IOError < Error
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建 IO 异常
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
 
+    # 操作系统异常
     class OSError < Error
       attr_reader :cause, :errno
+      # @!method cause
+      #  @return [SystemCallError] 系统调用错误
+      # @!method errno
+      #  @return [Integer] 操作系统错误代码
+
+      # 创建操作系统异常
+      #
+      # @param [Integer] code 操作系统错误代码
       def initialize(code)
         @errno = code
         @cause = SystemCallError.new(nil, code)
@@ -22,43 +39,73 @@ module QiniuNg
       end
     end
 
+    # 非预期的重定向错误
     class UnexpectedRedirectError < Error
+      # 创建非预期的重定向错误
       def initialize
         super('Unexpected redirect')
       end
     end
 
+    # 用户取消异常
     class UserCancelledError < Error
+      # 创建用户取消异常
       def initialize
         super('User canceled')
       end
     end
 
+    # JSON 错误
     class JSONError < Error
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建 JSON 错误
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
 
+    # 响应状态码错误
     class ResponseStatusCodeError < Error
       attr_reader :code
-      def initialize(code, qiniu_str)
+      # @!method code
+      #  @return [Integer] 操作系统错误代码
+
+      # 创建响应状态码错误
+      #
+      # @param [Integer] code 响应状态码
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(code, reason)
         @code = code
-        @message = qiniu_str
+        @message = reason
         super(@message.get_ptr)
       end
     end
 
+    # 未知错误
     class UnknownError < Error
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建未知错误
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
 
+    # Curl 错误
     class CurlError < Error
       attr_reader :curl_code, :error_kind
+      # @!method curl_code
+      #  @return [Integer] Curl 错误代码
+      # @!method error_kind
+      #  @return [Symbol] Curl 错误类型
+
+      # 创建 Curl 错误
+      #
+      # @param [Integer] curl_code Curl 错误代码
+      # @param [Symbol] error_kind Curl 错误类型，接受如下错误类型 `:qiniu_ng_resolve_error`, `:qiniu_ng_proxy_error`, `:qiniu_ng_ssl_error`, `:qiniu_ng_connection_error`, `:qiniu_ng_request_error`, `:qiniu_ng_response_error`, `:qiniu_ng_timeout_error`
       def initialize(curl_code, error_kind)
         @curl_code = curl_code
         @error_kind = case error_kind
@@ -76,43 +123,63 @@ module QiniuNg
       end
     end
 
+    # 无法删除非空存储空间
     class CannotDropNonEmptyBucketError < Error
+      # 创建无法删除非空存储空间错误
       def initialize
         super('Drop non empty bucket is not allowed')
       end
     end
 
+    # 非法的 MIME 错误
     class BadMIMEError < Error
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建非法的 MIME 错误
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
 
+    # 非法的上传凭证错误
+    #
+    # 总共有三种非法的上传凭证错误类，该类是他们共同的父类
     class InvalidUploadTokenError < Error
     end
 
+    # 非法的上传凭证格式错误
     class InvalidUploadTokenFormatError < InvalidUploadTokenError
+      # 创建非法的上传凭证格式错误
       def initialize
         super('Invalid upload token format')
       end
     end
 
+    # 上传凭证 Base64 解析错误
     class InvalidUploadTokenBase64DecodeError < InvalidUploadTokenError
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建上传凭证 Base64 解析错误
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
 
+    # 上传凭证 JSON 解析错误
     class InvalidUploadTokenJSONDecodeError < InvalidUploadTokenError
-      def initialize(qiniu_str)
-        @reason = qiniu_str
+      # 创建上传凭证 JSON 解析错误
+      #
+      # @param [Bindings::QiniuNgStr] reason 错误描述信息
+      def initialize(reason)
+        @reason = reason
         super(@reason.get_ptr)
       end
     end
   end
 
+  # @!visibility private
   def Error.wrap_ffi_function
     core_ffi = Bindings.const_get(:CoreFFI)
     return_values = yield
--- qiniu-ruby/lib/qiniu_ng/version.rb
+++ qiniu-ruby/lib/qiniu_ng/version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 module QiniuNg
+  # SDK 版本
   VERSION = '0.0.2'
 end
