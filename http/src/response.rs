b49f571b94da8cffbc9fa31a1adbc4e0fc9835b9
Bachue Zhou <bachue.shu@gmail.com>
2020-09-06 12:12:12 +0800

    now Response Body structure is not public

--- http/src/response.rs
+++ http/src/response.rs
@@ -6,10 +6,9 @@ use std::{
     fmt::Debug,
     fs::File,
     io::{
-        copy as io_copy, Error as IOError, ErrorKind as IOErrorKind, Read, Result as IOResult,
-        Seek, SeekFrom,
+        copy as io_copy, Cursor, Error as IOError, ErrorKind as IOErrorKind, Read,
+        Result as IOResult, Seek, SeekFrom,
     },
-    mem::take,
     net::IpAddr,
     result,
 };
@@ -18,23 +17,44 @@ use tempfile::tempfile;
 /// HTTP 响应状态码
 pub type StatusCode = u16;
 
-/// 实现了 Read 和 Debug 的 Trait
-pub trait ReadDebug: Read + Debug + Send + Sync {}
-
-impl<T: Read + Debug + Send + Sync> ReadDebug for T {}
+trait ReadSeekDebug: Read + Seek + Debug + Send + Sync {}
+impl<T: Read + Seek + Debug + Send + Sync> ReadSeekDebug for T {}
 
 /// HTTP 响应体
 #[derive(Debug)]
-pub enum Body {
-    Reader(Box<dyn ReadDebug>),
+pub struct Body(BodyInner);
+
+#[derive(Debug)]
+enum BodyInner {
+    SeekableReader(Box<dyn ReadSeekDebug>),
     File(File),
-    Bytes(Vec<u8>),
+    Bytes(Cursor<Vec<u8>>),
 }
 
 impl Default for Body {
     #[inline]
     fn default() -> Self {
-        Self::Bytes(Default::default())
+        Self(BodyInner::Bytes(Default::default()))
+    }
+}
+
+impl Read for Body {
+    fn read(&mut self, buf: &mut [u8]) -> IOResult<usize> {
+        match &mut self.0 {
+            BodyInner::SeekableReader(reader) => reader.read(buf),
+            BodyInner::File(file) => file.read(buf),
+            BodyInner::Bytes(bytes) => bytes.read(buf),
+        }
+    }
+}
+
+impl Seek for Body {
+    fn seek(&mut self, pos: SeekFrom) -> IOResult<u64> {
+        match &mut self.0 {
+            BodyInner::SeekableReader(reader) => reader.seek(pos),
+            BodyInner::File(file) => file.seek(pos),
+            BodyInner::Bytes(bytes) => bytes.seek(pos),
+        }
     }
 }
 
@@ -56,7 +76,7 @@ impl Default for Response {
         Self {
             status_code: 200,
             headers: Default::default(),
-            body: Body::Bytes(Default::default()),
+            body: Default::default(),
             server_ip: None,
             server_port: 80,
         }
@@ -144,27 +164,19 @@ impl Response {
         self.body
     }
 
-    /// 取出响应体
-    ///
-    /// 原响应中的响应体为替换为空
-    #[inline]
-    pub fn take_body(&mut self) -> Body {
-        take(&mut self.body)
-    }
-
     /// 复制响应体
     ///
     /// 该方法将尝试读取响应体，然后复制其内容
     pub fn clone_body(&mut self) -> IOResult<Body> {
         let content_length = self.try_to_get_content_length();
-        return match self.body_mut() {
-            Body::Reader(reader) => {
+        return match &mut self.body_mut().0 {
+            BodyInner::SeekableReader(reader) => {
                 let [body1, body2] = clone_body_from_reader(reader, content_length)?;
                 *self.body_mut() = body1;
                 Ok(body2)
             }
-            Body::File(file) => Ok(Body::File(file.try_clone()?)),
-            Body::Bytes(body) => Ok(Body::Bytes(body.to_owned())),
+            BodyInner::File(file) => Ok(Body(BodyInner::File(file.try_clone()?))),
+            BodyInner::Bytes(body) => Ok(Body(BodyInner::Bytes(body.to_owned()))),
         };
 
         fn clone_body_from_reader(
@@ -177,12 +189,18 @@ impl Response {
                     if content_length as usize != body.read_to_end(&mut buf)? {
                         return Err(IOError::from(IOErrorKind::UnexpectedEof));
                     }
-                    return Ok([Body::Bytes(buf.to_owned()), Body::Bytes(buf)]);
+                    return Ok([
+                        Body(BodyInner::Bytes(Cursor::new(buf.to_owned()))),
+                        Body(BodyInner::Bytes(Cursor::new(buf))),
+                    ]);
                 }
             }
             let mut file = tempfile()?;
             io_copy(body, &mut file)?;
-            Ok([Body::File(file.try_clone()?), Body::File(file)])
+            Ok([
+                Body(BodyInner::File(file.try_clone()?)),
+                Body(BodyInner::File(file)),
+            ])
         }
     }
 
@@ -191,14 +209,17 @@ impl Response {
         if let Some(content_length) = self.try_to_get_content_length() {
             return Ok(content_length);
         }
-        match self.body() {
-            Body::Bytes(body) => Ok(body.len().try_into().unwrap()),
-            Body::File(file) => Ok(file.metadata()?.len()),
-            Body::Reader(_) => match self.clone_body()? {
-                Body::Bytes(body) => Ok(body.len().try_into().unwrap()),
-                Body::File(file) => Ok(file.metadata()?.len()),
-                _ => panic!("Unexpected body type"),
-            },
+        return match &mut self.body_mut().0 {
+            BodyInner::Bytes(body) => Ok(body.get_ref().len().try_into().unwrap()),
+            BodyInner::File(file) => Ok(file.metadata()?.len()),
+            BodyInner::SeekableReader(body) => get_len_from_seekable(body),
+        };
+
+        fn get_len_from_seekable(body: &mut dyn Seek) -> IOResult<u64> {
+            let cur_offset = body.seek(SeekFrom::Current(0))?;
+            let len = body.seek(SeekFrom::End(0))?;
+            body.seek(SeekFrom::Start(cur_offset))?;
+            Ok(len)
         }
     }
 
@@ -263,15 +284,18 @@ impl ResponseBuilder {
 
     /// 设置数据流为 HTTP 响应体
     #[inline]
-    pub fn stream_as_body(mut self, body: impl Read + Debug + Send + Sync + 'static) -> Self {
-        self.inner.body = Body::Reader(Box::new(body));
+    pub fn seekable_stream_as_body(
+        mut self,
+        body: impl Read + Seek + Debug + Send + Sync + 'static,
+    ) -> Self {
+        self.inner.body = Body(BodyInner::SeekableReader(Box::new(body)));
         self
     }
 
     /// 设置二进制字节数组为 HTTP 响应体
     #[inline]
     pub fn bytes_as_body(mut self, body: impl Into<Vec<u8>>) -> Self {
-        self.inner.body = Body::Bytes(body.into());
+        self.inner.body = Body(BodyInner::Bytes(Cursor::new(body.into())));
         self
     }
 
@@ -279,7 +303,7 @@ impl ResponseBuilder {
     #[inline]
     pub fn file_as_body(mut self, mut body: File) -> IOResult<Self> {
         body.seek(SeekFrom::Start(0))?;
-        self.inner.body = Body::File(body);
+        self.inner.body = Body(BodyInner::File(body));
         Ok(self)
     }
 
