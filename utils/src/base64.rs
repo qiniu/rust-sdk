e35a674c842e1240798fa8399aca36d6bd1f563d
Bachue Zhou <bachue.shu@gmail.com>
2021-10-08 11:18:58 +0800

    fix ci failures

--- utils/src/base64.rs
+++ utils/src/base64.rs
@@ -9,13 +9,13 @@ pub fn urlsafe(data: &[u8]) -> String {
 }
 
 /// 以 URL 安全的方式，将指定的二进制数据编码为 Base64 字符串
-pub fn urlsafe_buf(data: &[u8], mut encoded: &mut String) {
-    base64::encode_config_buf(data, base64::URL_SAFE, &mut encoded)
+pub fn urlsafe_buf(data: &[u8], encoded: &mut String) {
+    base64::encode_config_buf(data, base64::URL_SAFE, encoded)
 }
 
 /// 以 URL 安全的方式，将指定的二进制数据编码为 Base64 字符串
-pub fn urlsafe_slice(data: &[u8], mut encoded: &mut [u8]) -> usize {
-    base64::encode_config_slice(data, base64::URL_SAFE, &mut encoded)
+pub fn urlsafe_slice(data: &[u8], encoded: &mut [u8]) -> usize {
+    base64::encode_config_slice(data, base64::URL_SAFE, encoded)
 }
 
 /// 以 URL 安全的方式，将指定的 Base64 字符串解码为二进制数据
@@ -24,11 +24,11 @@ pub fn decode(data: &[u8]) -> Result<Vec<u8>, DecodeError> {
 }
 
 /// 以 URL 安全的方式，将指定的 Base64 字符串解码为二进制数据
-pub fn decode_buf(data: &[u8], mut decoded: &mut Vec<u8>) -> Result<(), DecodeError> {
-    base64::decode_config_buf(data, base64::URL_SAFE, &mut decoded)
+pub fn decode_buf(data: &[u8], decoded: &mut Vec<u8>) -> Result<(), DecodeError> {
+    base64::decode_config_buf(data, base64::URL_SAFE, decoded)
 }
 
 /// 以 URL 安全的方式，将指定的 Base64 字符串解码为二进制数据
-pub fn decode_slice(data: &[u8], mut decoded: &mut [u8]) -> Result<usize, DecodeError> {
-    base64::decode_config_slice(data, base64::URL_SAFE, &mut decoded)
+pub fn decode_slice(data: &[u8], decoded: &mut [u8]) -> Result<usize, DecodeError> {
+    base64::decode_config_slice(data, base64::URL_SAFE, decoded)
 }
