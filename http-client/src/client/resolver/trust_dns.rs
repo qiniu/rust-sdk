13c5ac5d718fb6aa0d651fae4c3081d7488e2de1
Rong Zhou <zhourong@qiniu.com>
2023-08-23 06:22:44 +0000

    optimize resolver test cases

--- http-client/src/client/resolver/trust_dns.rs
+++ http-client/src/client/resolver/trust_dns.rs
@@ -25,18 +25,18 @@ type TrustDnsResolveResult<T> = Result<T, ResolveError>;
 impl TrustDnsResolver {
     /// 创建 [`trust-dns`](https://trust-dns.org/) 域名解析器
     #[inline]
-    pub async fn new(config: ResolverConfig, options: ResolverOpts) -> TrustDnsResolveResult<Self> {
-        Ok(Self {
-            resolver: resolver(config, options).await?,
-        })
+    pub async fn new(config: ResolverConfig, options: ResolverOpts) -> Self {
+        Self {
+            resolver: resolver(config, options).await,
+        }
     }
 
     /// 创建默认的 [`trust-dns`](https://trust-dns.org/) 域名解析器
     #[inline]
-    pub async fn default() -> TrustDnsResolveResult<Self> {
-        Ok(Self {
-            resolver: resolver(Default::default(), Default::default()).await?,
-        })
+    pub async fn default() -> Self {
+        Self {
+            resolver: resolver(Default::default(), Default::default()).await,
+        }
     }
 
     /// 通过系统的 system.conf 文件创建 [`trust-dns`](https://trust-dns.org/) 域名解析器
@@ -85,33 +85,72 @@ fn convert_trust_dns_error_to_response_error(err: ResolveError, opts: ResolveOpt
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::test_utils::{make_record_set, make_zone, start_mock_dns_server};
+    use anyhow::{anyhow, Result as AnyResult};
     use std::{
-        collections::HashSet,
-        error::Error,
-        net::{IpAddr, Ipv4Addr, Ipv6Addr},
-        result::Result,
+        collections::{HashMap, HashSet},
+        net::{IpAddr, Ipv4Addr, SocketAddr},
     };
+    use tokio::{net::UdpSocket, task::spawn};
     use trust_dns_resolver::config::{LookupIpStrategy, NameServerConfigGroup};
+    use trust_dns_server::{
+        authority::Catalog,
+        proto::rr::{
+            rdata::{A, CNAME},
+            Name, RData, RecordType,
+        },
+        ServerFuture,
+    };
 
-    const DOMAIN: &str = "dns.alidns.com";
-    const IPS: &[IpAddr] = &[
-        IpAddr::V4(Ipv4Addr::new(223, 5, 5, 5)),
-        IpAddr::V4(Ipv4Addr::new(223, 6, 6, 6)),
-        IpAddr::V6(Ipv6Addr::new(0x2400, 0x3200, 0, 0, 0, 0, 0, 1)),
-        IpAddr::V6(Ipv6Addr::new(0x2400, 0x3200, 0xbaba, 0, 0, 0, 0, 1)),
-    ];
+    const DEFAULT_TTL: u32 = 3600;
 
     #[tokio::test]
-    async fn test_trust_dns_resolver() -> Result<(), Box<dyn Error>> {
+    async fn test_trust_dns_resolver() -> AnyResult<()> {
+        let (dns_server_addr, dns_server) = mock_dns_server().await?;
+        let (shutdown_signal, dns_server) = dns_server.graceful();
+        let join_handle = spawn(dns_server);
+
         let mut opts = ResolverOpts::default();
         opts.ip_strategy = LookupIpStrategy::Ipv4AndIpv6;
         let resolver = TrustDnsResolver::new(
-            ResolverConfig::from_parts(None, vec![], NameServerConfigGroup::from_ips_clear(IPS, 53, true)),
+            ResolverConfig::from_parts(
+                None,
+                vec![],
+                NameServerConfigGroup::from_ips_clear([dns_server_addr.ip()].as_slice(), dns_server_addr.port(), true),
+            ),
             opts,
         )
-        .await?;
-        let ips = resolver.async_resolve(DOMAIN, Default::default()).await?;
-        assert_eq!(make_set(ips.ip_addrs()), make_set(IPS));
+        .await;
+
+        {
+            let ips = resolver.async_resolve("dns.alidns.com", Default::default()).await?;
+            assert_eq!(
+                make_set(ips.ip_addrs()),
+                make_set([
+                    IpAddr::V4(Ipv4Addr::new(2, 3, 4, 5)),
+                    IpAddr::V4(Ipv4Addr::new(3, 4, 5, 6)),
+                ])
+            );
+        }
+
+        {
+            let ips = resolver.async_resolve("alidns.com", Default::default()).await?;
+            assert_eq!(
+                make_set(ips.ip_addrs()),
+                make_set([IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4)),])
+            );
+        }
+
+        {
+            let ips = resolver.async_resolve("www.alidns.com", Default::default()).await?;
+            assert_eq!(
+                make_set(ips.ip_addrs()),
+                make_set([IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4)),])
+            );
+        }
+
+        shutdown_signal.shutdown().await;
+        join_handle.await??;
         Ok(())
     }
 
@@ -119,5 +158,51 @@ mod tests {
         let mut h = HashSet::new();
         h.extend(ips.as_ref());
         h
+    }
+
+    async fn mock_dns_server() -> AnyResult<(SocketAddr, ServerFuture<Catalog>)> {
+        let root_name = Name::from_str_relaxed("alidns.com")?;
+        let root_record_set = make_record_set(
+            root_name.to_owned(),
+            RecordType::A,
+            DEFAULT_TTL,
+            [(root_name.to_owned(), DEFAULT_TTL, RData::A(A::new(1, 2, 3, 4)))],
+        );
+        let sub_name = Name::from_str_relaxed("dns.alidns.com.")?;
+        let sub_record_set = make_record_set(
+            sub_name.to_owned(),
+            RecordType::A,
+            DEFAULT_TTL,
+            [
+                (sub_name.to_owned(), DEFAULT_TTL, RData::A(A::new(2, 3, 4, 5))),
+                (sub_name.to_owned(), DEFAULT_TTL, RData::A(A::new(3, 4, 5, 6))),
+            ],
+        );
+        let other_names = Name::from_str_relaxed("*.alidns.com.")?;
+        let other_record_set = make_record_set(
+            other_names.to_owned(),
+            RecordType::CNAME,
+            DEFAULT_TTL,
+            [(
+                other_names.to_owned(),
+                DEFAULT_TTL,
+                RData::CNAME(CNAME(root_name.to_owned())),
+            )],
+        );
+
+        let zone = make_zone(
+            root_name.to_owned(),
+            [
+                (root_name.to_owned(), RecordType::A, root_record_set),
+                (sub_name.to_owned(), RecordType::A, sub_record_set),
+                (other_names.to_owned(), RecordType::CNAME, other_record_set),
+            ],
+        )
+        .map_err(|err| anyhow!(err))?;
+        let udp_socket = UdpSocket::bind("127.0.0.1:0").await?;
+        let socket_addr = udp_socket.local_addr()?;
+        let mut zones = HashMap::with_capacity(2);
+        zones.insert(root_name, zone);
+        Ok((socket_addr, start_mock_dns_server(udp_socket, zones)))
     }
 }
